7f882167a05531d339f3d57e1b2046ac7b1f7e96
Roma Tolkachev <tolkachevroman@bk.ru>
2024-07-25 18:11:50 +0300

    before build

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -22,10 +22,13 @@ export const Hero: FunctionComponent = () => {
     return (
         <div className="my-gradient overflow-hidden h-[calc(100svh-79px)] w-full">
             <div className="bg-no-repeat min-h-[700px] h-full bg-[length:auto_100%]" style={backGroundStyle}>
-                <div className="container sm:-translate-x-[15%] md:-translate-x-[15%] lg:-translate-x-[20%] h-full flex items-center justify-center">
-                    <div className="max-w-[400px] md:max-w-[480px] xl:max-w-[680px] tracking-tight">
-                        <h1 className="pl-4 text-hero-main-small-mobile sm:pl-0 sm:text-hero-main text-white mb-4 uppercase font-black">нам<br/> доверяют<br/> по праву</h1>
-                        <p className="px-4 sm:px-0 text-hero-legend text-white mb-10">Мы - юридическая компания «In Jure», специализирующаяся на правовом сопровождении
+                <div className="hero-mask h-full w-full realtive opacity-60 md:hidden z-[1]"></div>
+                <div className="container sm:-translate-x-[15%] md:-translate-x-[15%] lg:-translate-x-[20%] top-0 h-full absolute z-[2] flex items-center justify-center">
+                    <div className="max-w-[400px] md:max-w-[480px] xl:max-w-[680px] tracking-tight ">
+                        <h1 className="pl-4 text-hero-main-small-mobile sm:pl-0 sm:text-hero-main text-white mb-4 uppercase font-black">нам<br/> доверяют<br/> по
+                            праву</h1>
+                        <p className="px-4 sm:px-0 text-hero-legend text-white mb-10">Мы - юридическая компания «In
+                            Jure», специализирующаяся на правовом сопровождении
                             бизнеса и на оказании помощи физическим лицам во всех регионах России.</p>
                         <div className='max-sm:mx-auto w-fit'>
                             <ButtonToVideo>смотреть видео</ButtonToVideo>
