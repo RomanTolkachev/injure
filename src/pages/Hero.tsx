3b324a859a7cd5a9542635e86f0a618f27bae681
Roma Tolkachev <tolkachevroman@bk.ru>
2024-07-28 09:33:55 +0300

    did landscape adaptive

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -24,12 +24,12 @@ export const Hero: FunctionComponent = () => {
   // };
 
   return (
-    <div className="my-gradient h-[calc(100svh-79px)] w-full overflow-hidden">
+    <div className="my-gradient h-[calc(100svh-79px)] min-h-[600px] w-full overflow-hidden">
       <div
-        className="bg-hero container h-full min-h-[700px] w-full bg-[length:auto_100%] bg-center bg-no-repeat lg:bg-right"
+        className="bg-hero container relative h-full w-full bg-[length:auto_100%] bg-center bg-no-repeat lg:bg-right"
         // style={backGroundStyle}
       >
-        <div className="hero-mask absolute left-0 z-[1] h-[calc(100svh-79px)] w-full opacity-60 sm:hidden"></div>
+        <div className="hero-mask absolute left-0 z-[1] h-[calc(100svh-79px)] min-h-[600px] w-full opacity-60 sm:hidden"></div>
         <div className="absolute top-0 z-[2] flex h-full items-center justify-center max-sm:right-1/2 max-sm:translate-x-1/2 sm:pl-4 md:pl-6 xl:pl-20">
           <div className="flex h-full max-w-[400px] flex-col justify-center tracking-tight max-sm:py-16 md:max-w-[480px] xl:max-w-[680px]">
             <h1 className="mb-4 pl-4 text-hero-main-small-mobile font-black uppercase text-white max-sm:mt-auto sm:pl-0 sm:text-hero-main">
