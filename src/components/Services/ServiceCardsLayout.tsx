34f0141688b0db84413a6267f75af030c0b5a3be
Roma Tolkachev <tolkachevroman@bk.ru>
2024-08-21 23:17:39 +0300

    animated services list

--- src/components/Services/ServiceCardsLayout.tsx
+++ src/components/Services/ServiceCardsLayout.tsx
@@ -1,23 +1,47 @@
 import { FunctionComponent } from "react";
 import { ServiceCard } from "./ServiceCard";
 import { services } from "../../services";
+import { motion } from "framer-motion";
+
+const parentVariants = {
+  start: {},
+  end: {
+    transition: {
+      staggerChildren: 0.3,
+      duration: 0.7,
+      delayChildren: 0.2,
+    },
+  },
+};
+
+const listVariants = {
+  start: { opacity: 0 },
+  end: { opacity: 1 },
+};
 
 export const ServiceCardsLayout: FunctionComponent = () => {
   const filtered = services.filter((item) => item.type === "physical");
 
   return (
-    <div
+    <motion.ul
+      variants={parentVariants}
+      initial="start"
+      animate="end"
       className={
         "grid-container mx-auto grid max-w-[1024px] grid-flow-dense auto-rows-fr grid-cols-3 gap-4"
       }
     >
       {filtered.map((item, key) => {
         return (
-          <div className={`h-full w-full ${item.size}`} key={key}>
+          <motion.li
+            variants={listVariants}
+            className={`h-full w-full ${item.size}`}
+            key={key}
+          >
             <ServiceCard serviceData={item} className={`h-full w-full`} />
-          </div>
+          </motion.li>
         );
       })}
-    </div>
+    </motion.ul>
   );
 };
