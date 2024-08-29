18cb3032f899e60173223dc1f8486584f81c53ee
Roma Tolkachev <tolkachevroman@bk.ru>
2024-08-29 22:54:52 +0300

    styles works. before mask refactoring

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 import { useScrollLock } from "../../services/hooks/useScrollLock";
-import { motion, useMotionValueEvent, useScroll } from "framer-motion";
+import { motion } from "framer-motion";
 import { useLocation } from "react-router-dom";
 import { Sprite } from "../Sprite";
 
@@ -22,6 +22,19 @@ const modalVariants = {
   },
 };
 
+const getScrollPosition = (ref: React.RefObject<any>): 0 | 0.5 | 1 => {
+  const containerHeight = ref.current!.clientHeight;
+  const scrollTop = Math.ceil(ref.current!.scrollTop);
+  const childrenHeight = ref.current!.children[0].clientHeight;
+  if (scrollTop === 0) {
+    return 0;
+  } else if (scrollTop + containerHeight >= childrenHeight) {
+    return 1;
+  } else {
+    return 0.5;
+  }
+};
+
 const handleShadow = (
   element: React.RefObject<HTMLElement>,
   scrollPosition: number,
@@ -29,20 +42,17 @@ const handleShadow = (
   const el = element.current!;
   switch (scrollPosition) {
     case 0: {
-      console.log("скролл вначале");
       el.classList.remove("top-mask-dark", "bot-top-mask-dark");
       el.classList.add("bottom-mask-dark");
       break;
     }
     case 1: {
-      console.log("домотали до конца");
       el.classList.add("top-mask-dark");
       el.classList.remove("bottom-mask-dark", "bot-top-mask-dark");
       break;
     }
     default: {
       if (scrollPosition === 0.5) {
-        console.log("сработал кейс середина");
         el.classList.remove("top-mask-dark", "bottom-mask-dark");
         el.classList.add("bot-top-mask-dark");
       }
@@ -67,27 +77,13 @@ const Modal: React.FunctionComponent<IModalProps> = ({
   // далее функция для определения тени
 
   const containerRef = useRef<HTMLDivElement>(null);
-  // const { scrollYProgress } = useScroll({ container: containerRef });
-
-  const getScrollPosition = (ref: React.RefObject<any>): 0 | 0.5 | 1 => {
-    const containerHeight = ref.current!.clientHeight;
-    const scrollTop = Math.ceil(ref.current!.scrollTop);
-    const childrenHeight = ref.current!.children[0].clientHeight;
-    if (scrollTop === 0) {
-      console.log("мы наверху");
-      return 0;
-    } else if (scrollTop + containerHeight >= childrenHeight) {
-      console.log("мы снизу");
-      return 1;
-    } else {
-      console.log("мы в середине");
-      return 0.5;
-    }
-  };
 
   useEffect(() => {
     const container = containerRef.current!;
-    if (container.clientHeight < container.children[0].clientHeight) {
+    const isScrollable: boolean =
+      container.clientHeight < container.children[0].clientHeight;
+
+    if (isScrollable) {
       handleShadow(containerRef, 0);
       const handleScroll = (): void =>
         handleShadow(containerRef, getScrollPosition(containerRef));
@@ -97,22 +93,6 @@ const Modal: React.FunctionComponent<IModalProps> = ({
       };
     }
   }, []);
-  //   if (
-  //     containerRef.current!.clientHeight <
-  //     containerRef.current!.children[0].clientHeight
-  //   ) {
-  //     handleShadow(containerRef, 0);
-  //   }
-  // }, []);
-  // useMotionValueEvent(scrollYProgress, "change", (latest) => {
-  //   if (
-  //     containerRef.current!.clientHeight >=
-  //     containerRef.current!.children[0].clientHeight
-  //   ) {
-  //     return;
-  //   }
-  //   console.log("сработал юз моушен вэлью");
-  //   handleShadow(containerRef, latest);
 
   // далее функции для закрытия модалки
 
@@ -153,19 +133,19 @@ const Modal: React.FunctionComponent<IModalProps> = ({
       >
         <div
           className={
-            "relative z-[12] my-auto h-full max-h-full w-full overflow-hidden rounded-3xl bg-my-deep-light py-10 sm:h-fit sm:pb-10 sm:pt-20 md:w-[700px]"
+            "relative z-[12] my-auto h-full max-h-full w-full overflow-hidden rounded-3xl bg-my-deep-light py-10 sm:pb-10 lg:h-fit lg:w-[700px] lg:pt-20"
           }
           ref={innerRef}
         >
           <div
             ref={containerRef}
-            className={`h-fit max-h-[calc(100dvh-5rem)] overflow-y-auto sm:max-h-[calc(100svh-7.5rem)]`}
+            className={`h-fit max-h-[calc(100dvh-5rem)] w-full overflow-y-auto lg:max-h-[calc(100svh-7.5rem)]`}
           >
             {children}
           </div>
           <div
             className={
-              "absolute right-[-10px] top-[-12px] h-[70px] p-3 text-my-deep-gray hover:cursor-pointer sm:right-[7px] sm:top-[7px]"
+              "absolute right-[-10px] top-[-12px] h-[70px] p-3 text-my-deep-gray hover:cursor-pointer lg:right-[7px] lg:top-[7px]"
             }
             onClick={closeModal}
           >
