a3f47cb0210d155e4c6ef6788157141ea7f6a427
Roma Tolkachev <tolkachevroman@bk.ru>
2024-09-01 16:04:39 +0300

    tapClick function refactoring

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -3,7 +3,7 @@ import { CallUs } from "../components/CallUs/CallUs";
 import { team } from "../services/team";
 import { SectionDescription } from "../components/Header/SectionDescription";
 import { ServiceCardsLayout } from "../components/Services/ServiceCardsLayout";
-import { motion } from "framer-motion";
+import { ServiceNav } from "../components/Services/ServiceNav";
 
 export const Services: FunctionComponent = () => {
   const [currentType, setCurrentType] = useState<"business" | "physical">(
@@ -17,44 +17,7 @@ export const Services: FunctionComponent = () => {
         className={"mb-10 2xl:mb-14"}
       />
       <nav className={"pt-5"}>
-        <div
-          className={
-            "border-my-gray-light mx-auto grid w-fit grid-cols-2 gap-2 overflow-hidden rounded-3xl border p-1 text-sm font-bold sm:text-xl md:text-3xl [&>*]:first-letter:capitalize"
-          }
-        >
-          <motion.button
-            animate={{
-              boxShadow:
-                currentType === "physical"
-                  ? "-4px 3px 7px 3px rgba(34, 60, 80, 0.2)"
-                  : "-4px 3px 7px 3px rgba(34, 60, 80, 0)",
-              color: currentType === "physical" ? "#211C1C" : "#B7B7B7",
-            }}
-            className={
-              "pointer-events-auto text-nowrap rounded-2xl p-4 text-center"
-            }
-            onClick={() => setCurrentType("physical")}
-            onTap={() => setCurrentType("physical")}
-          >
-            частным клиентам
-          </motion.button>
-          <motion.button
-            animate={{
-              boxShadow:
-                currentType === "business"
-                  ? "-4px 3px 7px 3px rgba(34, 60, 80, 0.2)"
-                  : "-4px 3px 7px 3px rgba(34, 60, 80, 0)",
-              color: currentType === "business" ? "#211C1C" : "#B7B7B7",
-            }}
-            className={
-              "pointer-events-auto text-nowrap rounded-2xl p-4 text-center"
-            }
-            onClick={() => setCurrentType("business")}
-            onTap={() => setCurrentType("business")}
-          >
-            бизнесу
-          </motion.button>
-        </div>
+        <ServiceNav currentType={currentType} setCurrentType={setCurrentType} />
       </nav>
       <div>
         <ServiceCardsLayout currentType={currentType} />
