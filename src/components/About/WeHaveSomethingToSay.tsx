3087f7aab52d4f0c1d4ae0a3ab68655e970b1a97
Roma Tolkachev <tolkachevroman@bk.ru>
2024-09-09 19:52:51 +0300

    git anything to tell adaptive done

--- src/components/About/WeHaveSomethingToSay.tsx
+++ src/components/About/WeHaveSomethingToSay.tsx
@@ -27,10 +27,12 @@ const childrenVariants = {
 
 export const WeHaveSomethingToSay: FunctionComponent = () => {
   return (
-    <div className={"mx-auto w-full max-w-screen-lg px-16"}>
-      <div className={"rounded-2xl bg-white py-16"}>
+    <div className={"mx-auto w-full max-w-screen-lg sm:px-16"}>
+      <div className={"rounded-2xl bg-white py-6 sm:py-8 md:py-12 lg:py-16"}>
         <motion.div
-          className={"relative mx-auto mb-12 h-44 w-full"}
+          className={
+            "relative mx-auto mb-4 h-24 w-full sm:mb-8 sm:h-28 md:mb-10 md:h-36 lg:mb-12 lg:h-44"
+          }
           variants={parentVariants}
           initial="start"
           animate="end"
@@ -51,12 +53,16 @@ export const WeHaveSomethingToSay: FunctionComponent = () => {
         </motion.div>
         <h3
           className={
-            "mx-auto mb-8 w-fit text-4xl font-bold first-letter:capitalize"
+            "mx-auto mb-2 w-fit text-2xl font-bold first-letter:capitalize sm:mb-4 sm:text-3xl md:mb-6 md:text-4xl lg:mb-8 lg:text-5xl"
           }
         >
           нам есть что сказать
         </h3>
-        <p className={"mx-auto w-fit max-w-[500px] text-center"}>
+        <p
+          className={
+            "md:text-md mx-auto w-fit max-w-[500px] px-4 text-center text-sm lg:px-0 lg:text-lg"
+          }
+        >
           Наша цель - защита прав и интересов клиентов и предоставление
           высококачественных услуг благодаря честному и законному подходу к
           бизнесу.
